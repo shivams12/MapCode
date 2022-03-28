2a4bfe3876314083a7d8888a4629b639feaee3d4
shivam12 <shivamsahni77@gmail.com>
2022-03-28 11:39:03 +0530

    googlemaps

--- GoogleMaps.java
+++ GoogleMaps.java
@@ -6,28 +6,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class automate {
+public class GoogleMaps {
 	public static void main(String[] args) throws InterruptedException {
+
 		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/maps");
 		driver.findElement(By.id("searchboxinput")).sendKeys("San Francisco, California");
-		Thread.sleep(1000);
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("searchbox-searchbutton")));
 		driver.findElement(By.id("searchbox-searchbutton")).click();
-		Thread.sleep(2000);
 		String value = "37.7577627,-122.4726194";
 		driver.getPageSource().contains(value);
-		Thread.sleep(1000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[alt='Directions']")));
 		driver.findElement(By.cssSelector("img[alt='Directions']")).click();
-		Thread.sleep(2000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[id='sb_ifc51']>input")));
 		driver.findElement(By.cssSelector("div[id='sb_ifc51']>input")).sendKeys("chico");
 		driver.findElement(By.cssSelector("div[id='directions-searchbox-0']>button:nth-of-type(1)")).click();
-		Thread.sleep(2000);
-		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='siAUzd-neVct']>div")));
 		List<WebElement> titles = driver.findElements(By.cssSelector("div[class='siAUzd-neVct']>div"));
-		for(int i=0; i<titles.size();i++) {
+		for (int i = 0; i < titles.size(); i++) {
 			System.out.println(titles.get(i).getText());
 		}
 	}
